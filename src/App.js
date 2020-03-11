2252000b0717907ca866faad93733eb5a08c88b3
christinehhh <[ga48vum@mytum.de]>
2020-03-11 15:09:56 +0100

    Circle Buttons Group added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Map from "./Components/Images/map.png";
+import Music from "./Components/Images/music.png";
+import Settings from "./Components/Images/setting.png";
+import LightOn from "./Components/Images/Turn on light.png";
+import LightOff from "./Components/Images/Turn off light.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const picsHeight = window.innerHeight / 5;
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div
+        className="btn-group-vertical"
+        role="group"
+        aria-label="Vertical button group"
+      >
+        <button type="button" className="btn btn-circle">
+          <img height={picsHeight} src={Map} alt="" />
+        </button>
+        <button type="button" className="btn btn-circle">
+          <img height={picsHeight} src={Music} alt="" />
+        </button>
+        <button type="button" className="btn btn-circle">
+          <img height={picsHeight} src={Settings} alt="" />
+        </button>
+        <button type="button" className="btn btn-circle">
+          <img height={picsHeight} src={LightOn} alt="" />
+        </button>
+        <button type="button" className="btn btn-circle">
+          <img height={picsHeight} src={LightOff} alt="" />
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
