f01ce242236deb060c25748358ad9360145512be
christinehhh <[ga48vum@mytum.de]>
2020-03-14 22:59:59 +0100

    Added an unfunctionable ProgressBar

--- src/Components/musics.jsx
+++ src/Components/musics.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { getMusics } from "./fakeMusicService";
 import Like from "./common/like";
 import Play from "./common/play";
+import ProgressBar from "./progressBar";
 
 class Music extends Component {
   state = {
@@ -26,44 +27,47 @@ class Music extends Component {
 
   render() {
     return (
-      <table className="table table-hover table-sm table-striped">
-        <thead className="thead-dark">
-          <tr>
-            <th> </th>
-            <th>Title</th>
-            <th></th>
-            <th>Artist</th>
-            <th>Album</th>
-            <th></th>
-            <th>Time</th>
-            <th>Genre</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.musics.map(music => (
-            <tr key={music._id}>
-              <td>{this.state.musics.indexOf(music) + 1}</td>
-              <th>{music.title}</th>
-              <th>
-                <Play
-                  played={music.played}
-                  onClick={() => this.handlePlay(music)}
-                />
-              </th>
-              <td>{music.artist}</td>
-              <td>{music.album}</td>
-              <td>
-                <Like
-                  liked={music.liked}
-                  onClick={() => this.handleLike(music)}
-                />
-              </td>
-              <td>{music.time}</td>
-              <td>{music.genre.name}</td>
+      <React.Fragment>
+        <table className="table table-hover table-sm table-striped">
+          <thead className="thead-dark">
+            <tr>
+              <th> </th>
+              <th>Title</th>
+              <th></th>
+              <th>Artist</th>
+              <th>Album</th>
+              <th></th>
+              <th>Time</th>
+              <th>Genre</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.musics.map(music => (
+              <tr key={music._id}>
+                <td>{this.state.musics.indexOf(music) + 1}</td>
+                <th>{music.title}</th>
+                <th>
+                  <Play
+                    played={music.played}
+                    onClick={() => this.handlePlay(music)}
+                  />
+                </th>
+                <td>{music.artist}</td>
+                <td>{music.album}</td>
+                <td>
+                  <Like
+                    liked={music.liked}
+                    onClick={() => this.handleLike(music)}
+                  />
+                </td>
+                <td>{music.time}</td>
+                <td>{music.genre.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <ProgressBar />
+      </React.Fragment>
     );
   }
 }
